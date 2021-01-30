959981266fea040037037f573ec6314c510ff73f
Michael Gilmour <52269382+mgilmour-se@users.noreply.github.com>
2021-01-30 14:24:01 -0700

    fix all of jacks stuff

--- GGJ2021-Project/puzzles/BoxKeys.gd
+++ GGJ2021-Project/puzzles/BoxKeys.gd
@@ -1,90 +1,75 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var boxes = []
-var boxScenes = []
+
 enum Contents {NOTHING, GLUE, SHOCK, DOORKEY}
 
-
-
 class Box:
-	var xPos = 0
-	var yPos = 0
-	var height = 0
-	var width = 0
-	var id = 0
-	var whatsInside = 0
+	var id # int
+	var whatsInside # enum Contents
+	var dime # Vector2
+	var posi # Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	generateBoxes(10) # 10 is an arbitrary number; decided later
-	print(boxes[0].xPos)
-	print(boxes[0].yPos)
-	get_node("balls").position = Vector2(boxes[0].xPos, boxes[0].yPos)
-	
+	var children = get_children()
+	var static_bodies = []
+	for child in children:
+		if typeof(StaticBody2D):
+			static_bodies.append(child)
+	generateBoxes(static_bodies)
 
-func generateBoxes(numBoxes):
-	# First for loop generates all necessary box data
-	for n in range(numBoxes):
-		var newBox = Box.new()
-		newBox.id = n
-		if n == 0:
-			newBox.whatsInside = Contents.DOORKEY
-		elif n == 1 || n == 2:
-			newBox.whatsInside = Contents.GLUE
-		elif n == 3:
-			newBox.whatsInside = Contents.SHOCK
-		else:
-			newBox.whatsInside = Contents.NOTHING
-		generateDimensions(newBox)
-		boxes.append(newBox)
-		generateBoxPositions()
+func generateBoxes(bodies):
+	for n in range(bodies.size()):
+		print(bodies[n])
+		var box = generateBox(n, decideContents(n))
+		boxes.append(box)
+		bodies[n].position = box.posi
 		
-		var newBoxScene = load("res://puzzles/Box.tscn")
-		var newBoxSceneInstance = newBoxScene.instance()
-		var name = str("Box",n)
-		newBoxSceneInstance.set_name(name)
-		boxScenes.append(newBoxSceneInstance)
-	# Add the child to the scene 
-	# This is done after so that the box's ready function will apply all the necessary transforms
+func decideContents(num):
+	if num == 0:
+		return Contents.DOORKEY
+	elif num == 1 || num == 2:
+		return Contents.GLUE
+	elif num == 3:
+		return Contents.SHOCK
+	else:
+		return Contents.NOTHING
 	
-
-func generateDimensions(box):
-	# generates a random height and width from 5-15
-	box.height = randi() % 11 + 5
-	box.width = randi() % 11 + 5
+func generateDimensions():
+	# generates a random width and height from 5-15
+	randomize()
+	return Vector2(randi() % 11 + 5, randi() % 11 + 5)
 
 func generatePosition(box):
-	# generates two random positions 0-100
 	randomize()
-	box.xPos = randi() % 851
-	box.yPos = randi() % 451
+	var pos = Vector2(randi() % 851,  randi() % 451)
+	var valid = boxes.size() == 0
+	while !valid:
+		valid = true
+		var pyrange = range(pos.y, pos.y + box.dime.y)
+		for compare in boxes:
+			var cyrange = range(compare.posi.y, compare.posi.y + compare.dime.y)
+			if range(compare.posi.x, compare.posi.x + compare.dime.x).has(pos.x) && join(pyrange, cyrange).size() < pyrange.size() + cyrange.size():
+				valid == false
+			elif range(compare.posi.x - compare.dime.x, compare.posi.x).has(pos.x) && join(pyrange, cyrange).size() < pyrange.size() + cyrange.size():
+				valid == false
+			if !valid:
+				pos = Vector2(randi() % 851,  randi() % 451)
+				break
+	return pos
 
-func generateBoxPositions():
-	for box in boxes:
-		if box.id == boxes[0].id:
-			# if it's the first box, just give it a position
-			generatePosition(box)
-		else:
-			# Generate a random position, and then check for position validity.
-			generatePosition(box)
-			# while !checkPositionValidity (the position is false / not valid), generate a new position  
-			while !checkPositionValidity(box):
-				generatePosition(box)
-
-func checkPositionValidity(box):
-	for boxToCheck in boxes:
-		if box == boxToCheck:
-			pass
-			# axis-aligned bounding box collision checking
-		elif box.xPos + box.width > boxToCheck.xPos && box.xPos < boxToCheck.xPos + boxToCheck.width && box.yPos + box.height > boxToCheck.yPos && box.yPos < boxToCheck.yPos + boxToCheck.height:
-			return false
-		else:
-			return true
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func generateBox(num, contents):
+	var box = Box.new()
+	box.id = num
+	box.whatsInside = contents
+	box.dime = generateDimensions()
+	box.posi = generatePosition(box)
+	return box
+	
+func join(arr1, arr2):
+	var result = arr1
+	for a2 in arr2:
+		if !arr1.has(a2):
+			result.append(a2)
+	return result
