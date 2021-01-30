dac27816b37ae96c7ccb3809cdd3b0205b1d5a14
cbrown3 <4447329+cbrown3@users.noreply.github.com>
2021-01-29 20:14:52 -0500

    Base Project
    -Beginning of networking

---
+++ GGJ2021-Project/interface/Menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func _on_CreateButton_pressed():
+	Network.create_server("Player 1")
+	_load_game()
+
+func _on_JoinButton_pressed():
+	Network.connect_to_server("Player 2")
+	_load_game()
+
+func _load_game():
+	get_tree().change_scene('res://Game.tscn')
